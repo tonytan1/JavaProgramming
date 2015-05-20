612f4ac41634cf92dc418b87e3eb50bf86217809
tony.tan <tanxiucai1@gmail.com>
2015-05-20 18:01:33 +0800

    ListExecutionTimes...pending

---
+++ src/ListExecutionTimes.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tony.tan on 5/20/2015.
+ */
+
+import java.util.*;
+
+public class ListExecutionTimes {
+    public static void main(String[] args){
+        String operationType = "Average get and set time";
+        int length = 1000;
+        ArrayList<Double> alist = new ArrayList<>();
+
+
+    }
+
+}
