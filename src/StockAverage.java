8f8bbb0e7aecb2075623b2b4cf1d1b25784b7a7e
Tony Tan <tanxiucai1@gmail.com>
2015-05-21 22:05:57 +0800

    some review

--- src/StockAverage.java
+++ src/StockAverage.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class StockAverage {
     public static void main(String[] args){
         Scanner stdIn = new Scanner(System.in);
-        ArrayList<Double> stocks = new ArrayList<>();
+        ArrayList<Double> stocks = new ArrayList<Double>();
         //The Double class wraps a value of the primitive type double
         // in an object.An object of type Double
         // contains a single field whose type is double.
