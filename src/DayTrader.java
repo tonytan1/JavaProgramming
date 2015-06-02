076c372baeb586c8d1cc2d846d37df9297d6ae02
Tony Tan <tanxiucai1@gmail.com>
2015-06-02 22:20:09 +0800

    fix bugs

--- src/DayTrader.java
+++ src/DayTrader.java
@@ -5,16 +5,18 @@
  */
 public class DayTrader {
     public static void main(String[] args){
-        double balance = 1000;
-        double moneyInvested;
-        double moneyReturned;
         int win=0;
         int fail=0;
         int day;
         for(int simulation=0; simulation<100;simulation++ ){
+            double balance = 1000;
+            double moneyInvested;
+            double moneyReturned;
+
             for(day=1; day<=90; day++){
-                if(balance<100|| balance > 2000)continue;{
-                    int result = (balance>1000?win++:fail++);
+                if(balance<100|| balance > 2000){
+                    System.out.println("\nSimulation: "+(simulation+1) +"\n");
+                    break;
                 }
                 moneyInvested = 500;
                 balance -= moneyInvested;
@@ -22,6 +24,7 @@ public class DayTrader {
                 balance+=moneyReturned;
                 System.out.print("\nINVESTMENT:"+(int)moneyInvested+"\tRETURNED: "+(int)moneyReturned+"\tBALANCE: "+(int)balance);
             }
+            int result = (balance>2000?win++:fail++);
         }
         System.out.println("\nWIN: "+win+"\tFAIL:"+fail);
     }
