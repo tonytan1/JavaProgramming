12906e13a2de664e9008eb7c00f0b235b723d7b1
tony.tan <tanxiucai1@gmail.com>
2015-06-24 17:20:46 +0800

    producer-consumer case update

---
+++ src/Concurrency/Producer1.java
@@ -0,0 +1,25 @@
+package Concurrency;
+
+/**
+ * Created by tony.tan on 6/24/2015.
+ */
+public class Producer1 implements Runnable{
+    private FileMock mock;
+
+    private Buffer buffer;
+
+    public Producer1(FileMock mock, Buffer buffer){
+        this.mock = mock;
+        this.buffer = buffer;
+    }
+
+    @Override
+    public void run() {
+        buffer.setPendingLines(true);
+        while (mock.hasMoreLines()){
+            String line=mock.getLine();
+            buffer.insert(line);
+        }
+        buffer.setPendingLines(false);
+    }
+}
